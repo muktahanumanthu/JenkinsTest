a0e4e70bc3b65a586dc2a1b90052c8677c901228
muktahanumanthu <43252238+muktahanumanthu@users.noreply.github.com>
2021-05-03 23:29:54 -0700

    Add files via upload

---
+++ pipeline2.groovy
@@ -0,0 +1,66 @@
+import groovy.*
+
+num = param.number
+
+echo $num
+
+if (main() == "SUCCESS"){
+    echo("Main Success")
+} else {
+    echo("Main Failure")
+    currentBuild.result = "UNSTABLE"
+    return
+}
+
+
+def main() {
+    def retVal = "SUCCESS"
+	try {
+		node {
+			echo("Starting the pipeline")
+			stage('Checkout the code') {
+
+                
+                echo("Options ::")
+                echo("Branch for code checkout : $env.BRANCH_NAME")
+
+				echo("Checking out the code")
+				gitCheckout("your git URL", env.BRANCH_NAME)
+                
+                // Check if version already exisits
+				}
+            stage('Build') {
+                sh """
+                  ./build.sh $num
+                """
+            }
+
+
+	}
+		
+	} catch (Exception ex) {
+		echo("an exception while execution", ex)
+		retVal = "FAILURE"
+	}
+	return retVal
+}
+
+
+// Functions
+
+// Functions
+def gitCheckout(def gitURL, def gitBranch){
+
+    checkout changelog: false, poll: false, scm: [
+        $class: 'GitSCM',
+        branches: [[name: gitBranch]],
+        doGenerateSubmoduleConfigurations: false,
+        submoduleCfg: [],
+        userRemoteConfigs: [
+            [
+                credentialsId: 'adityy_github',
+                url: "${gitURL}"
+            ]
+        ]
+    ]
+}
