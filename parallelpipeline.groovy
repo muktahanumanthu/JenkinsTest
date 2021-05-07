dead8df0ce2d638322c8e6eee081440c4a7d2568
muktahanumanthu <43252238+muktahanumanthu@users.noreply.github.com>
2021-05-06 22:09:02 -0700

    Update parallelpipeline.groovy

--- parallelpipeline.groovy
+++ parallelpipeline.groovy
@@ -1,15 +1,19 @@
 node() {
     echo "Testing the parallel execution in stages"
-    
+    sleep 30
     parallel deploy1 : {
         stage('DeployDev') {
             echo "This is deploy dev stage of parallel"
             sleep 20
+            echo "Finishing stage 1"
+            
         }
     }, deploy: {
         stage('DeployDev2') {
+            echo "Starting stage 2"
             echo "This is deploy dev2 stage of parallel execution"
             sleep 10
+            echo "Finishing stage 2"
         }
     }
 }
