3a5344c039d39555143266e2485cd46050d14585
muktahanumanthu <43252238+muktahanumanthu@users.noreply.github.com>
2021-05-06 22:45:09 -0700

    Create firstDeclarativePipeline.groovy

---
+++ firstDeclarativePipeline.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+
+    stages {
+        stage("Build") {
+            steps {
+                echo "Testing declarative pipeline..."
+            }
+        }
+
+        stage("Test") {
+            steps {
+                echo "Executing tests..."
+                echo 1
+            }
+        }
+    }
+}
+
